730ce8249e7c2d3de5ef38a42401221cf70ddeb5
radha226 <radhasingh226@gmail.com>
2019-04-12 11:10:34 +0530

    slider

--- js/common.js
+++ js/common.js
@@ -2,7 +2,6 @@
  
 });
 function isOnScreen(){
-  // console.log($.fn);
   $.fn.isOnScreen = function(){
     var height = this.outerHeight();
     var width = this.outerWidth();
@@ -11,7 +10,6 @@ function isOnScreen(){
     }
 
     var win = $(window);
-    console.log(win);
     var viewport = {
       top : win.scrollTop(),
       left : win.scrollLeft()
@@ -41,22 +39,92 @@ function isOnScreen(){
   };
 }
 $(document).ready(function(){
-  // console.log('check');
-  
   isOnScreen();
-  $('.firm').each(function () {
-    var cur = $(this)
-    if (cur.isOnScreen(0.15, 0.15)) {
-      if (!cur.hasClass('animated')) {
-        cur.addClass('animated fadeInUp')
-      }
-    }
-    $(window).scroll(function () {
+  //navigaion
+  $('.mobile-menu a').click(function(e){
+    event.preventDefault();
+    $(this).parents('header').addClass('active');
+    $(this).parents('header').find('nav').slideToggle();
+  })
+  $('.banner').slick({
+    infinite: true,
+    autoplay: true,
+    fade: true,
+    cssEase: 'linear',
+    dots: false,
+    arrows: false,
+  });
+  // Animation
+    window.setTimeout(function () {
+      $('.banner').slick('slickPause');
+      $('.slide-thumb').addClass('animated fadeIn');
+    }, 0);
+    window.setTimeout(function () {
+      $('.round').addClass('animated slideInLeft');
+    }, 200);
+    window.setTimeout(function () {
+      $('.slide-content .robot').addClass('animated slideInLeft');
+    }, 800);
+    window.setTimeout(function () {
+      $('.slide-content h3').addClass('animated slideInLeft');
+    }, 1000);
+    window.setTimeout(function () {
+      $('.slide-content h2').addClass('animated slideInLeft');
+    }, 1500);
+    window.setTimeout(function () {
+      $('.slide-content a').addClass('animated slideInLeft');
+    }, 2000);
+    // Start slider once all elements are loaded
+    window.setTimeout(function () {
+      $('.banner').slick('slickPlay');
+    }, 2500);
+
+    // Before slide transition, remove all animation elements on previous slide
+    $('.banner').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      $('.round, .slide-content h2,.slide-content .robot, .slide-content h3,.slide-content a ').removeClass('animated slideInLeft');
+    });
+
+    // After slide transition, animate boxes and plus sign
+    $('.banner').on('afterChange', function (event, slick, currentSlide) {
+      window.setTimeout(function () {
+        $('.banner').slick('slickPause');
+        $('.slide-thumb').addClass('animated fadeIn');
+      }, 0);
+      window.setTimeout(function () {
+        $('.round').addClass('animated slideInLeft');
+      }, 200);
+      window.setTimeout(function () {
+        $('.slide-content .robot').addClass('animated slideInLeft');
+      }, 800);
+      window.setTimeout(function () {
+        $('.slide-content h3').addClass('animated slideInLeft');
+      }, 1000);
+      window.setTimeout(function () {
+        $('.slide-content h2').addClass('animated slideInLeft');
+      }, 1500);
+      window.setTimeout(function () {
+        $('.slide-content a').addClass('animated slideInLeft');
+      }, 2000);
+      // Start slider once all elements are loaded
+      window.setTimeout(function () {
+        $('.banner').slick('slickPlay');
+      }, 2500);
+    });
+
+    // Page animation
+    $('nav ul li').each(function () {
+      var cur = $(this);
       if (cur.isOnScreen(0.15, 0.15)) {
         if (!cur.hasClass('animated')) {
-          cur.addClass('animated fadeInUp')
+          cur.addClass('animated fadeInUp');
         }
       }
-    })
-  })
+      $(window).scroll(function () {
+      if (cur.isOnScreen(0.15, 0.15)) {
+        if (!cur.hasClass('animated')) {
+          cur.addClass('animated fadeInUp');
+        }
+      }
+    });
+  });
 })
